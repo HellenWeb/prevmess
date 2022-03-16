5fc1463a728b22b068711a9bad97ec6632946c85
HellenWeb <artemkondratyev23@gmail.com>
2022-03-16 21:25:22 +1000

    Added files to the project twist and modified them

---
+++ prevmess/client/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.scss'
+
+function App() {
+  return (
+    <div className="container mx-auto">
+      <h1 className="text-center">Home</h1>
+    </div>
+  );
+}
+
+export default App;
