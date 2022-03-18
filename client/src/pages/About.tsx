e53c435d607b0fa5cbb47a2ea25bb736afe22126
HellenWeb <artemkondratyev23@gmail.com>
2022-03-18 22:49:32 +1000

    Made a page Home and About Front-End

--- client/src/pages/About.tsx
+++ client/src/pages/About.tsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 export const About = () => {
     return (
-        <div className="container mx-auto">
-            <h1>About</h1>
+        <div>
+            <Navbar />
+            <div className="intro">
+                <div className="container mx-auto">
+                    <div className="intro__inner mt-40 text-center">
+                        <h1 className='text-6xl'>HellenWeb</h1>
+                        <p className='m-7 opacity-70'>
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. dolor sit amet consectetur
+                        </p>
+                        <div className="flex justify-center">
+                            <a href="https://github.com/HellenWeb"><button className="p-2 pl-5 mr-2 pr-5 bg-gray-500 text-gray-100 text-sm rounded-lg focus:border-4 border-gray-300">Github</button></a>
+                            <a href="https://t.me/YungHellen"><button className="p-2 pl-5 pr-5 bg-blue-500 text-gray-100 text-sm rounded-lg focus:border-4 border-blue-300">Telegram</button></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <Footer />
         </div>
     )
 }
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 export const Home = () => {
     return (
-        <div className="container mx-auto">
-            <h1>Home</h1>
+        <div>
+            <Navbar />
+            <div className="intro">
+                <div className="container mx-auto">
+                    <div className="intro__inner mt-40 text-center">
+                        <h1 className='text-6xl' style={{'fontWeight': '5'}}>PrevMess</h1>
+                        <p className='mt-6 opacity-75'>
+                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa eum aspernatur totam?
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <Footer />
         </div>
     )
 }
