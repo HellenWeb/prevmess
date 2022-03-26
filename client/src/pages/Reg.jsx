20c7b8ad89733eb19ace4aaaaa12706e41ce65b9
HellenWeb <artemkondratyev23@gmail.com>
2022-03-26 23:58:58 +1000

    Finished the logic of registration and authorization

--- client/src/pages/Reg.jsx
+++ client/src/pages/Reg.jsx
@@ -1,12 +1,34 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+import { useHttp } from '../hooks/http.hook'
+import { render } from '@testing-library/react'
 
 export const Reg = () => {
+  const {loading, request, error, clearError} = useHttp()
+  const [form, setForm] = useState({
+    email: '', password: ''
+  })
 
-    function registerHandler() {
-        return fetch('https://127.0.0.1:8000/')
-    }
+  const changeHandler = event => {
+    setForm({ ...form, [event.target.name]: event.target.value })
+  }
+
+  const registerHandler = async () => {
+    try {
+      const data = await request('/api/submit/register', 'POST', {...form})
+      return 'OK'
+    } catch (e) {}
+  }
+
+  const loginHandler = async () => {
+    try {
+      const data = await request('/api/submit/login', 'POST', {...form})
+    } catch (e) {}
+  }
+
+  render()
 
     return (
         <div>
@@ -17,30 +39,39 @@ export const Reg = () => {
                     <header className="mb-3 text-2xl font-bold">Create your profile</header>
                     <div className="w-full rounded-2xl bg-gray-50 px-4 ring-2 ring-gray-200">
                     <input
+                        name="email"
                         type="text"
                         placeholder="Email"
+                        value={form.email}
+                        onChange={changeHandler}
                         className="my-3 w-full border-none bg-transparent outline-none focus:outline-none"
                     />
                     </div>
                     <div className="w-full rounded-2xl bg-gray-50 px-4 ring-2 ring-gray-200">
                     <input
+                        name="password"
                         type="password"
+                        value={form.password}
+                        onChange={changeHandler}
                         placeholder="Password"
                         className="my-3 w-full border-none bg-transparent outline-none focus:outline-none"
                     />
                     </div>
                     <div className="flex justify-center">
                         <button
+                        onClick={registerHandler}
                         className="w-full rounded-2xl border-b-4 border-b-blue-600 bg-blue-500 py-3 font-bold text-white hover:bg-blue-400 active:translate-y-[0.125rem] active:border-b-blue-400 mr-2"
                         >
                         CREATE ACCOUNT
                         </button>
                         <button
+                        onClick={loginHandler}
                         className="w-full rounded-2xl border-b-4 border-b-blue-600 bg-blue-500 py-3 font-bold text-white hover:bg-blue-400 active:translate-y-[0.125rem] active:border-b-blue-400"
                         >
                         LOG IN
                         </button>
                     </div>
+                    <meta name='csrf-token' content='{{ csrf_token() }}' />
                 </div>
             </div>
             </div>
