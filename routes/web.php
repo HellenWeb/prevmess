9e5dc6bc52eb5a71da796b6545750ed7bf5b484f
HellenWeb <artemkondratyev23@gmail.com>
2022-04-28 18:09:30 +1000

    Added login and logout form

--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,4 @@
 use Illuminate\Support\Facades\Route;
 
 Route::post('/api/submit/register', 'App\Http\Controllers\RequestLog@submit')->name('register');
+Route::post('/api/submit/login', 'App\Http\Controllers\RequestLog@login')->name('login');
